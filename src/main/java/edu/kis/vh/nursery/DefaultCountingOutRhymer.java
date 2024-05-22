fe126be96db633cd8da69581f903797bcbd8976a
Darvel <marsonyteam@gmail.com>
2024-05-22 21:44:08 +0200

    Dział 2 Stałe i annotacje

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,31 +1,39 @@
 package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
+    private static final int CAPACITY = 12;
+    private static final int EMPTY_STACK_INDICATOR = -1;
+    private static final int FULL_STACK_INDICATOR = CAPACITY - 1;
+    private static final int DEFAULT_VALUE = -1;
 
-    private int[] NUMBERS = new int[12];
-
-    public int total = -1;
+    private int[] numbers = new int[CAPACITY];
+    private int total = EMPTY_STACK_INDICATOR;
 
     public void countIn(int in) {
-        if (!isFull()) NUMBERS[++total] = in;
+        if (!isFull()) {
+            numbers[++total] = in;
+        }
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == EMPTY_STACK_INDICATOR;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == FULL_STACK_INDICATOR;
     }
 
     protected int peekaboo() {
-        if (callCheck()) return -1;
-        return NUMBERS[total];
+        if (callCheck()) {
+            return DEFAULT_VALUE;
+        }
+        return numbers[total];
     }
 
     public int countOut() {
-        if (callCheck()) return -1;
-        return NUMBERS[total--];
+        if (callCheck()) {
+            return DEFAULT_VALUE;
+        }
+        return numbers[total--];
     }
-
 }
--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -5,29 +5,39 @@ import edu.kis.vh.nursery.factory.RhymersFactory;
 
 class RhymersDemo {
 
+    private static final int RHYMERS_COUNT = 4;
+    private static final int ELEMENTS_TO_COUNT_IN = 15;
+    private static final int RANDOM_BOUND = 20;
+
     public static void main(String[] args) {
         RhymersFactory factory = new DefaultRhymersFactory();
         
-        DefaultCountingOutRhymer[] rhymers = { factory.getStandardRhymer(), factory.getFalseRhymer(),
-                factory.getFIFORhymer(), factory.getHanoiRhymer()};
+        DefaultCountingOutRhymer[] rhymers = {
+            factory.getStandardRhymer(),
+            factory.getFalseRhymer(),
+            factory.getFIFORhymer(),
+            factory.getHanoiRhymer()
+        };
         
-        for (int i = 1; i < 15; i++)
-            for (int j = 0; j < 3; j++)
+        for (int i = 1; i < ELEMENTS_TO_COUNT_IN; i++) {
+            for (int j = 0; j < RHYMERS_COUNT - 1; j++) {
                 rhymers[j].countIn(i);
+            }
+        }
         
         java.util.Random rn = new java.util.Random();
-        for (int i = 1; i < 15; i++)
-            rhymers[3].countIn(rn.nextInt(20));
+        for (int i = 1; i < ELEMENTS_TO_COUNT_IN; i++) {
+            rhymers[RHYMERS_COUNT - 1].countIn(rn.nextInt(RANDOM_BOUND));
+        }
         
-        for (int i = 0; i < rhymers.length; i++) {
-            while (!rhymers[i].callCheck())
+        for (int i = 0; i < RHYMERS_COUNT; i++) {
+            while (!rhymers[i].callCheck()) {
                 System.out.print(rhymers[i].countOut() + "  ");
+            }
             System.out.println();
         }
         
         System.out.println("total rejected is "
-                + ((HanoiRhymer) rhymers[3]).reportRejected());
-        
+                + ((HanoiRhymer) rhymers[RHYMERS_COUNT - 1]).reportRejected());
     }
-    
 }
