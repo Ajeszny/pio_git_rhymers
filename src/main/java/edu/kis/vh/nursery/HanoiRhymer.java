6219a890f5348ed656242be22a41ce17566e67ae
Ajeszny <245107@edu.p.lodz.pl>
2024-06-19 16:10:36 +0200

    Dokumentacja klasy

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,11 +1,27 @@
 package edu.kis.vh.nursery;
 
+/**
+ * HanoiRhymer
+ */
 public class HanoiRhymer extends DefaultCountingOutRhymer {
+    /**
+     * Total rejected
+     */
     private int totalRejected = 0;
+
+    /**
+     *
+     * @return total rejected calls
+     */
     public int reportRejected() {
         return totalRejected;
     }
-    
+
+    /**
+     *
+     * @param in in
+     *           
+     */
 	@Override
     public void countIn(int in) {
         if (!callCheck() && in > peekaboo())
